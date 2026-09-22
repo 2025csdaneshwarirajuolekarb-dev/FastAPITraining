a1f0bb1ffafa61e489533cc67fbd9cba830cc203
daneshwari <2025cs_daneshwarirajuolekar_b@nie.ac.in>
2026-09-22 15:21:39 +0530

    basemodel

--- day/day2/FastAPIPractice/main.py
+++ day/day2/FastAPIPractice/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI() 
 @app.get("/")
@@ -13,3 +14,27 @@ def about():
 @app.get("/health")
 def health():
     return {"status": "ok"}
+#POST request
+@app.post("/create")
+def create_something():
+    return {"item": "created" }
+#path parameters
+@app.get("/student/{usn}")
+def get_result(usn):
+    return {"usn": usn,"Result":"Distinction"}
+
+#path parameters with type hint
+@app.get("/candidate/{rollno}")
+def get_candidate(rollno: int):
+    return {"rollno": rollno,"Result":"Distinction","type":str(type(rollno))}
+
+#pydantic model
+class Item(BaseModel):
+    name: str
+    price: float
+    in_stock: bool = True
+
+@app.post("/items")
+def create_item(item:Item):
+    return {"received":item, "total_price":item.price*1.18}
+
