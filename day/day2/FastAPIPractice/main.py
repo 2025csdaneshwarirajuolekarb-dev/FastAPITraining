3ff607cda78b82c424433c357362b83e0d2fe911
daneshwari <2025cs_daneshwarirajuolekar_b@nie.ac.in>
2026-09-22 12:53:48 +0530

    adding multipe routes

--- day/day2/FastAPIPractice/main.py
+++ day/day2/FastAPIPractice/main.py
@@ -2,5 +2,14 @@ from fastapi import FastAPI
 
 app = FastAPI() 
 @app.get("/")
-def read_root():
-    return {"Hello": "World","number":44,"is_fun":True}   
+def home():
+    return {"page": "Home"}
+# to get this about page, you can go to http:// and put another /about at the end of the url and write about
+   
+@app.get("/about") 
+def about():
+    return {"page": "About","author":"Daneshwari"}
+
+@app.get("/health")
+def health():
+    return {"status": "ok"}
